264f7b46ba4bf1650cc658c8c15b9649a1e7c8a0
kuti gbolahan <kutigbolahan@ymail.com>
2020-03-12 15:54:42 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter_sound/flutter_sound.dart';
 import 'package:webfeed/webfeed.dart';
 import 'package:http/http.dart' as http;
 
-
-
-final url ='https://itsallwidgets.com/podcast/feed';
+final url = 'https://itsallwidgets.com/podcast/feed';
 
 void main() => runApp(MyApp());
 
@@ -29,17 +27,17 @@ class EpisodesPage extends StatelessWidget {
     return Scaffold(
       body: FutureBuilder(
         future: http.get(url),
-        builder: (context, AsyncSnapshot<http.Response>snapshot){
-          if(snapshot.hasData){
+        builder: (context, AsyncSnapshot<http.Response> snapshot) {
+          if (snapshot.hasData) {
             final response = snapshot.data;
-            if(response.statusCode == 200){
-              final rssString=response.body;
-              var rssFeed= RssFeed.parse(rssString);
-              return EpisodeTile(rssFeed: rssFeed);
+            if (response.statusCode == 200) {
+              final rssString = response.body;
+              var rssFeed = RssFeed.parse(rssString);
+              return EpisodeListView(rssFeed: rssFeed);
             }
-          }else{
+          } else {
             return Center(
-             child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(),
             );
           }
         },
@@ -48,8 +46,8 @@ class EpisodesPage extends StatelessWidget {
   }
 }
 
-class EpisodeTile extends StatelessWidget {
-  const EpisodeTile({
+class EpisodeListView extends StatelessWidget {
+  const EpisodeListView({
     Key key,
     @required this.rssFeed,
   }) : super(key: key);
@@ -58,23 +56,35 @@ class EpisodeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListWheelScrollView(
-      itemExtent: 42,
-      diameterRatio: 2.5, 
-      
-      useMagnifier: true,
-      magnification: 1.5,
-      children: rssFeed.items.map((i) => ListTile(
-        title: Text(i.title),
-        
-      )).toList());
+    return ListView(
+        children: rssFeed.items
+            .map((i) => ListTile(
+                  title: Text(i.title),
+                  subtitle: Text(
+                    i.description,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  onTap: (){
+                 Navigator.of(context).push(MaterialPageRoute(builder: (_)=>
+                   PlayerPage(item: i)
+                 ));
+                  },
+                ))
+            .toList());
   }
 }
 
 class PlayerPage extends StatelessWidget {
+  PlayerPage({this.item});
+  final RssItem item;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Player()));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item.title),
+      ),
+      body: SafeArea(child: Player()));
   }
 }
 
@@ -122,8 +132,8 @@ class _PlaybackButtonsState extends State<PlaybackButtons> {
   void _stop() async {
     await _sound.stopPlayer();
     setState(() {
-            _isPlaying = false;
-          });
+      _isPlaying = false;
+    });
   }
 
   void _play() async {
@@ -132,15 +142,13 @@ class _PlaybackButtonsState extends State<PlaybackButtons> {
       ..listen((e) {
         if (e != null) {
           print(e.currentPosition);
-          
-       setState(()=>   playPosition = e.currentPosition/e.duration);
 
-         
+          setState(() => playPosition = e.currentPosition / e.duration);
         }
       });
-       setState(() {
-            _isPlaying = true;
-          });
+    setState(() {
+      _isPlaying = true;
+    });
   }
 
   void _fastForward() {}
